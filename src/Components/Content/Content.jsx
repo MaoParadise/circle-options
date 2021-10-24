f16e556bfdfc9c0264f367cdf07f8c88864ee64d
Carlos Andres Pasten Bravo <carlos92.pastenbravo@gmail.com>
2021-10-24 18:54:18 -0300

    extra

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,5 +1,5 @@
 import './Content.css'
-import { React } from 'react';
+import React from 'react';
 
 
 
@@ -9,9 +9,10 @@ const Content = () => {
 
 
     return(
+        <React.Fragment>
         <section className="grid">
-            <div className="col-span-1 bg-gray-500 relative h-96 pr-2">
-                <div className="relative ml-8 mt-2 w-64 h-64 rounded-full bg-black transform rotate-custom">
+            <div className="col-span-1 bg-transparent relative h-96 pr-2">
+                <div className="relative ml-8 mt-20 w-64 h-64 rounded-full bg-black transform rotate-custom">
                     <div className="absolute transform border-2 border-gray-800 first-figure rounded-full bg-blue-300 w-64 h-64 "></div> 
                     <div className="absolute transform border-2 border-gray-800 second-figure rounded-full bg-yellow-500 w-64 h-64"></div>
                     <div className="absolute transform border-2 border-gray-800 third-figure rounded-full bg-blue-300 w-64 h-64  "></div>
@@ -22,7 +23,12 @@ const Content = () => {
                 <div className="bg-purple-400 w-2/3 h-80 absolute top-0 right-8 opacity-50 mr-20 mt-32"></div>
             </div>
         </section>
-        
+        <section className="grid grid-cols-4 pt-20">
+            <div className="col-span-3 bg-blue-200 h-56"></div>
+            <div className="col-span-1 bg-blue-400 h-56"></div>
+            <div className="col-span-4 bg-pink-300 h-64"></div>
+        </section>
+        </React.Fragment>
     );
 }
 
